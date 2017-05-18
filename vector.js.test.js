74a24a44e37fd35498963e43e5e70294cc73af96
Sidney Harrell <sidney.harrell@gmail.com>
2017-05-18 15:21:12 -0400

    got abstracted test working

--- vector.js.test.js
+++ vector.js.test.js
@@ -1,33 +1,18 @@
 var vectors = require('./vector.js');
 
-var vector1 = new vectors.Vector(3, 4);
-var vector2 = new vectors.Vector(10, 20);
-
-if (vector1.x === 3 && vector1.y === 4) {
-  console.log("element assignment and retrieval: PASSED");
-} else {
-  console.log("element assignment and retrieval: FAILED");
-}
-
-if (vector1.length() === 4) {
-  console.log("length method: PASSED");
-} else {
-  console.log("length method: FAILED");
-}
-
-vector1.add(vector2);
-
-console.log(vector1.x);
-var evaltest = function (test, description) {
-  if (test()) {
+var evaltest = function (obj, test, description) {
+  if (test(obj)) {
     console.log(description + ": PASSED")
   } else {
     console.log(description + ": FAILED")
   }
 }
 
-var retrieval = function () {
-  return vector1.x === 3 && vector1.y === 4;
-}
+var vector1 = new vectors.Vector(3, 4);
 
-evaltest(retrieval, "element assignment and retrieval");
+var retrieval = function (vector) {
+  return (vector.x === 3 && vector.y === 4);
+}
+evaltest(vector1, retrieval, "element assignment and retrieval");
+
+var vector2 = new vectors.Vector(10, 20);
