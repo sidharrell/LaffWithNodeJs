d2fcc8adca6c852679a68656a933e9d468e8ba02
Sidney Harrell <sidney.harrell@gmail.com>
2017-05-18 14:39:35 -0400

    added vector add method

--- vector.js
+++ vector.js
@@ -4,9 +4,19 @@ function Vector(x, y) {
   this.length = function () {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
+  this.add = function (vector) {
+    this.x += vector.x;
+    this.y += vector.y;
+  }
 }
 
 var vector1 = new Vector(3, 4);
+var vector2 = new Vector(10, 20);
+
 
 console.log(vector1.x);
 console.log(vector1.length());
+
+vector1.add(vector2);
+
+console.log(vector1.x);
