3323cca6b8e0d1d1e3984bcc330f75c8d3ff1199
Sidney Harrell <sidney.harrell@gmail.com>
2017-05-18 14:31:20 -0400

    added length method

--- vector.js
+++ vector.js
@@ -1,8 +1,12 @@
 function Vector(x, y) {
   this.x = x;
   this.y = y;
+  this.length = function () {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
 }
 
-var vector1 = new Vector(2, 3);
+var vector1 = new Vector(3, 4);
 
 console.log(vector1.x);
+console.log(vector1.length());
