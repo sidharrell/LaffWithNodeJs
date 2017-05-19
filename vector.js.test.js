fe44dc0ee7cc386c1abf0e408a68d828cfd62f55
Sidney Harrell <sidney.harrell@gmail.com>
2017-05-19 02:36:02 -0400

    added add test

--- vector.js.test.js
+++ vector.js.test.js
@@ -18,6 +18,11 @@ evaltest(vector1, retrieval, "element assignment and retrieval");
 var lengthtest = function (vector) {
   return vector.length() === 5;
 }
-
 evaltest (vector1, lengthtest, "length method test")
+
+var addtest = function (vector) {
+  return (vector.x == 13 && vector.y == 24);
+}
 var vector2 = new vectors.Vector(10, 20);
+vector1.add(vector2);
+evaltest (vector1, addtest, "addition method test");
