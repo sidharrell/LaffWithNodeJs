f0b00b79d7e4c16202840a9931cda04472fff19c
Sidney Harrell <sidney.harrell@gmail.com>
2017-05-21 18:04:24 -0400

    refactored and removed legacy tests

--- test/all.js
+++ test/all.js
@@ -4,6 +4,11 @@ exports['test that logs all failures'] = function(assert) {
   var vector = new vectors.Vector(3, 4);
   assert.equal(vector.x, 3, 'test x assignment and retrieval');
   assert.equal(vector.y, 4, 'test y assignment and retrieval');
+  assert.equal(vector.length(), 5, 'test length method');
+  var vector2 = new vectors.Vector(10, 20);
+  vector.add(vector2);
+  assert.equal(vector.x, 13, 'test x after addition method');
+  assert.equal(vector.y, 24, 'test y after addition method')
 }
 
 if (module == require.main) require('test').run(exports)
