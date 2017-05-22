f6980df5fe2aa835b3d0aa8774ffc9d658a4bb53
Sidney Harrell <sidney.harrell@gmail.com>
2017-05-21 22:31:09 -0400

    refactored to allow copying. moved to factory method pattern.

--- test/all.js
+++ test/all.js
@@ -2,19 +2,24 @@
 exports['test that logs all failures'] = function(assert) {
   var vectors = require('../vector.js');
   var testelements = [3, 4];
-  var vector = new vectors.Vector(testelements);
+  var vectorfactory = new vectors.VectorFactory();
+  var vector = vectorfactory.createVector( testelements );
+  //var vector = new vectors.Vector(testelements);
   assert.equal(vector.elements[0], 3, 'test x assignment and retrieval');
   assert.equal(vector.elements[1], 4, 'test y assignment and retrieval');
   assert.equal(vector.length(), 2, 'test length method');
   assert.equal(vector.magnitude(), 5, 'test magnitude method');
   var testelements2 = [10, 20]
-  var vector2 = new vectors.Vector(testelements2);
+  var vector2 = vectorfactory.createVector( testelements2 );
   vector.add(vector2);
   assert.equal(vector.elements[0], 13, 'test x after addition method');
   assert.equal(vector.elements[1], 24, 'test y after addition method')
   vector.scale(10);
   assert.equal(vector.elements[0], 130, 'test x after scaling method');
-  assert.equal(vector.elements[1], 240, 'test y after scaling method')
+  assert.equal(vector.elements[1], 240, 'test y after scaling method');
+  vectorcopy = vectorfactory.copyVector( vector );
+  assert.equal(vectorcopy.elements[0], 130, 'test x after copy method');
+  assert.equal(vectorcopy.elements[1], 240, 'test y after copy method');
 }
 
 if (module == require.main) require('test').run(exports)
--- vector.js
+++ vector.js
@@ -1,31 +1,40 @@
-module.exports = {
-    Vector: function (elements) {
-    this.elements = elements;
-    this.magnitude = function () {
-      sum = 0;
-      fLen = this.length();
-      for (i = 0; i < fLen; i++) {
-        sum += this.elements[i] * this.elements[i];
-      }
-      return Math.sqrt(sum);
+function Vector(elements) {
+  this.elements = elements;
+  this.magnitude = function () {
+    sum = 0;
+    fLen = this.length();
+    for (i = 0; i < fLen; i++) {
+      sum += this.elements[i] * this.elements[i];
     }
-    this.length = function () {
-      return this.elements.length;
-    }
-    this.scale = function (scalingfactor) {
-      fLen = this.length();
-      for (i = 0; i < fLen; i++) {
-        this.elements[i] *= scalingfactor;
-      }
-    }
-    this.add = function (vector) {
-      fLen = this.length();
-      if (fLen !== vector.length()) {
-        return "error, cannot add vectors of unequal length";
-      }
-      for (i = 0; i < fLen; i++) {
-        this.elements[i] += vector.elements[i];
-      }
+    return Math.sqrt(sum);
+  }
+  this.length = function () {
+    return this.elements.length;
+  }
+  this.scale = function (scalingfactor) {
+    fLen = this.length();
+    for (i = 0; i < fLen; i++) {
+      this.elements[i] *= scalingfactor;
     }
   }
-};
+  this.add = function (vector) {
+    fLen = this.length();
+    if (fLen !== vector.length()) {
+      return "error, cannot add vectors of unequal length";
+    }
+    for (i = 0; i < fLen; i++) {
+      this.elements[i] += vector.elements[i];
+    }
+  }
+}
+function VectorFactory() {
+  this.createVector = function ( elements ) {
+    this.vectorClass = Vector;
+    return new this.vectorClass( elements );
+  }
+  this.copyVector = function ( vector ) {
+    this.vectorClass = Vector;
+    return new this.vectorClass( vector.elements );
+  }
+}
+module.exports = { VectorFactory };
